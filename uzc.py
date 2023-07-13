fd7abdf6108d590d3496bb63d3969ed873fcd4a2
huroshokunin <1CJK1207@cc.u-tokai.ac.jp>
2023-07-13 17:02:59 +0900

    微修正

--- uzc.py
+++ uzc.py
@@ -1,169 +1,129 @@
-# Import necessary modules
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import filedialog
 import json
 
-# Define a class for the app
-
 
 class TreeviewApp(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-
-        # Set window title, size and make it non-resizable
         self.master.title("Umamusume Zenkan Checker")
         self.master.geometry("1070x420")
         self.master.resizable(width=False, height=False)
 
-        # Load data from JSON file
-        self.list_grade = self.load_data()
-
-        # Create frames to hold the checkboxes and treeview
-        self.frames_grade, self.frame_treeview = self.create_frame()
-
-        self.create_menubar()
-
-        # Initialize an empty list to hold checked items
+        self.race_data = self.load_race_data_from_json()
         self.checked_items = []
         self.checkboxes = {}
 
-        # Create tabs for each grade and add checkboxes for races
-        for i, _ in enumerate(self.list_grade):
-            self.create_grade_tab(self.frames_grade[i], self.list_grade[i])
-
-        # Create treeview and scrollbar
+        self.frames_grade, self.frame_treeview = self.create_frames()
+        self.create_menubar()
+        self.create_grade_tabs()
         self.create_treeview(self.frame_treeview)
         self.create_scrollbar(self.frame_treeview)
 
-    # Function to load data from JSON file
-    def load_data(self):
-        list_grade = []
+    def load_race_data_from_json(self):
         with open('TrophyRoom.json', encoding='utf-8') as f:
             data = json.load(f)
-            data_g1 = data['G1']
-            data_g2 = data['G2']
-            data_g3 = data['G3']
-            list_grade.append(data_g1)
-            list_grade.append(data_g2)
-            list_grade.append(data_g3)
-        return list_grade
+        return [data[grade] for grade in ['G1', 'G2', 'G3']]
 
-    # Function to create frames for holding checkboxes and treeview
+    def create_frames(self):
+        frame_grade = self.create_grade_frames()
+        frame_treeview = self.create_treeview_frame()
+        return frame_grade, frame_treeview
 
-    def create_frame(self):
-        # Create Grade frames
-        # frame_g1, frame_g2, frame_g3 is in frame_grade
-        frames_grade = []
+    def create_grade_frames(self):
         frame_grade = tk.Frame(self.master)
         frame_grade.grid(row=0, columnspan=3, sticky=tk.NSEW)
+        frames = [self.create_grade_frame(frame_grade, i) for i in range(3)]
+        return frames
 
-        frame_g1 = tk.Frame(frame_grade)
-        frame_g2 = tk.Frame(frame_grade)
-        frame_g3 = tk.Frame(frame_grade)
-        frame_g1.grid(row=1, column=0, padx=5, sticky=tk.NW)
-        frame_g2.grid(row=1, column=1, padx=5, sticky=tk.NW)
-        frame_g3.grid(row=1, column=2, padx=5, sticky=tk.NW)
-        frames_grade.append(frame_g1)
-        frames_grade.append(frame_g2)
-        frames_grade.append(frame_g3)
+    def create_grade_frame(self, parent_frame, i):
+        frame = tk.Frame(parent_frame)
+        frame.grid(row=1, column=i, padx=5, sticky=tk.NW)
+        tk.Label(
+            frame,
+            text=f'G{i+1}',
+            font=("", 12, "bold"),
+            bd=5).grid(
+            sticky=tk.NW)
+        return frame
 
-        # Create Grade labels
-        label_g1 = tk.Label(frame_g1, text='G1', font=("", 12, "bold"), bd=5)
-        label_g2 = tk.Label(frame_g2, text='G2', font=("", 12, "bold"), bd=5)
-        label_g3 = tk.Label(frame_g3, text='G3', font=("", 12, "bold"), bd=5)
-        label_g1.grid(sticky=tk.NW)
-        label_g2.grid(sticky=tk.NW)
-        label_g3.grid(sticky=tk.NW)
-
-        # Create Treeview frames
+    def create_treeview_frame(self):
         frame_treeview = tk.Frame(self.master)
         frame_treeview.grid(row=2, column=0, columnspan=3, sticky=tk.NS)
-
-        return frames_grade, frame_treeview
+        return frame_treeview
 
     def create_menubar(self):
-        def save_file():
-            filename = filedialog.asksaveasfilename(
-                title="名前を付けて保存", filetypes=[
-                    ("Json files", "*.json")], initialdir="./", defaultextension=".json")
-            if filename:
-                with open(filename, 'w', encoding='utf-8') as outfile:
-                    json.dump(
-                        self.checked_items,
-                        outfile,
-                        indent=2,
-                        ensure_ascii=False)
-
-        def read_file():
-            filename = filedialog.askopenfilename(
-                title="ファイルを開く", filetypes=[
-                    ("Json files", "*.json")], initialdir="./")
-            if filename:
-                with open(filename, 'r', encoding='utf-8') as infile:
-                    items = json.load(infile)
-                    # Clear all checkboxes
-                    for checkbox in self.checkboxes.values():
-                        checkbox.deselect()
-                    # Check the loaded items
-                    for item in items:
-                        if item['Name'] in self.checkboxes:
-                            self.checkboxes[item['Name']].select()
-                            self.handle_checkbox(item)
-
         menubar = tk.Menu(self.master)
         self.master.config(menu=menubar)
-
         setting = tk.Menu(menubar, tearoff=0)
         menubar.add_cascade(label='ファイル', menu=setting)
-        setting.add_command(label='保存', command=save_file)
-        setting.add_command(label='読み込み', command=read_file)
+        setting.add_command(label='保存', command=self.save_file)
+        setting.add_command(label='読み込み', command=self.read_file)
         setting.add_separator()
         setting.add_command(label='終了', command=quit)
 
-    # Function to create tabs for each grade and add checkboxes for races
-    def create_grade_tab(self, frame, list_races):
-        # Calculate the number of tabs
-        checkbox_max = 15
-        tab_count = len(list_races) // checkbox_max
-        if len(list_races) % checkbox_max != 0:
-            tab_count += 1
+    def save_file(self):
+        filename = filedialog.asksaveasfilename(
+            title="名前を付けて保存",
+            filetypes=[("Json files", "*.json")],
+            initialdir="./",
+            defaultextension=".json"
+        )
+        if filename:
+            with open(filename, 'w', encoding='utf-8') as outfile:
+                json.dump(
+                    self.checked_items,
+                    outfile,
+                    indent=2,
+                    ensure_ascii=False)
 
-        # 15races in list_checkboxes
-        list_checkboxes = [list_races[i:i + checkbox_max]
-                           for i in range(0, len(list_races), checkbox_max)]
+    def read_file(self):
+        filename = filedialog.askopenfilename(
+            title="ファイルを開く",
+            filetypes=[("Json files", "*.json")],
+            initialdir="./"
+        )
+        if filename:
+            with open(filename, 'r', encoding='utf-8') as infile:
+                items = json.load(infile)
+                for checkbox in self.checkboxes.values():
+                    checkbox.deselect()
+                for item in items:
+                    if item['Name'] in self.checkboxes:
+                        self.checkboxes[item['Name']].select()
+                        self.handle_checkbox_click(item)
 
+    def create_grade_tabs(self):
+        for frame, races in zip(self.frames_grade, self.race_data):
+            self.create_tabs_with_checkboxes(frame, races)
+
+    def create_tabs_with_checkboxes(self, frame, races):
+        races_per_tab = 15
+        race_sublists = [races[i:i + races_per_tab]
+                         for i in range(0, len(races), races_per_tab)]
         notebook = ttk.Notebook(frame)
-        list_tab = []
-        rows = 0
-        columns = 0
-        for tab_num in range(tab_count):
-            # Create tabs frame
-            tab = tk.Frame(notebook)
-            list_tab.append(tab)
-
-            notebook.add(list_tab[tab_num], text=f'Page{tab_num+1}')
-        for tab_num, list_checkbox in enumerate(list_checkboxes):
-            for race_num, checkbox in enumerate(list_checkbox):
-                if (race_num % 3 == 0):
-                    rows += 1
-                    columns = 0
-
-                # Create a Checkbutton object and store it
-                # in the checkboxes dictionary
-                checkbox_race = tk.Checkbutton(
-                    list_tab[tab_num],
-                    text=checkbox['Name'],
-                    command=lambda checkbox=checkbox: self.handle_checkbox(checkbox))
-                checkbox_race.grid(row=rows, column=columns, sticky=tk.NW)
-                self.checkboxes[checkbox['Name']] = checkbox_race
-
-                columns += 1
+        for i, sublist in enumerate(race_sublists):
+            self.create_tab_with_checkboxes(notebook, sublist, i + 1)
         notebook.grid()
 
-    # Function to create treeview
+    def create_tab_with_checkboxes(self, notebook, races, tab_num):
+        tab = tk.Frame(notebook)
+        notebook.add(tab, text=f'Page{tab_num}')
+        for i, race in enumerate(races):
+            row, column = divmod(i, 3)
+            self.create_checkbox(tab, race, row, column)
+
+    def create_checkbox(self, parent, race, row, column):
+        checkbox = tk.Checkbutton(
+            parent,
+            text=race['Name'],
+            command=lambda race=race: self.handle_checkbox_click(race)
+        )
+        checkbox.grid(row=row, column=column, sticky=tk.NW)
+        self.checkboxes[race['Name']] = checkbox
+
     def create_treeview(self, frame):
-        # Create treeview
         self.tree = ttk.Treeview(frame, show='headings')
         self.tree['columns'] = (
             'Phase',
@@ -174,48 +134,31 @@ class TreeviewApp(tk.Frame):
             'CourseType',
             'Distance',
             'DistanceType',
-            'Handed')
-
-        # Set column widths and headings
-        self.tree.column('Phase', width=100, minwidth=50)
-        self.tree.column('Schedule', width=70, minwidth=60)
-        self.tree.column('Name', minwidth=50)
-        self.tree.column('Grade', width=40, minwidth=20)
-        self.tree.column('Place', minwidth=20)
-        self.tree.column('CourseType', width=40, minwidth=20)
-        self.tree.column('Distance', width=70, minwidth=20)
-        self.tree.column('DistanceType', width=60, minwidth=20)
-        self.tree.column('Handed', width=30, minwidth=20)
-
-        self.tree.heading('Phase', text='フェーズ')
-        self.tree.heading('Schedule', text='開催時期')
-        self.tree.heading('Name', text='レース名')
-        self.tree.heading('Grade', text='グレード')
-        self.tree.heading('Place', text='開催地')
-        self.tree.heading('CourseType', text='コース')
-        self.tree.heading('Distance', text='距離')
-        self.tree.heading('DistanceType', text='距離区分')
-        self.tree.heading('Handed', text='回り')
-
+            'Handed'
+        )
+        for column, width in zip(
+            self.tree['columns'], [
+                100, 70, 50, 40, 20, 40, 70, 60, 30]):
+            self.tree.column(column, width=width, minwidth=50)
+        for column, text in zip(
+            self.tree['columns'], [
+                'フェーズ', '開催時期', 'レース名', 'グレード', '開催地', 'コース', '距離', '距離区分', '回り']):
+            self.tree.heading(column, text=text)
         self.tree.grid(sticky=tk.NS)
 
-    # Function to create scrollbar
     def create_scrollbar(self, frame):
-        # Create scrollbar
         scrollbar = ttk.Scrollbar(
             frame, orient='vertical', command=self.tree.yview)
         scrollbar.grid(row=0, column=1, sticky=tk.NS)
         self.tree.configure(yscrollcommand=scrollbar.set)
 
-    # Function to handle checkbox clicks and update the treeview
-    def handle_checkbox(self, checkbox):
-        if checkbox in self.checked_items:
-            self.checked_items.remove(checkbox)
+    def handle_checkbox_click(self, race):
+        if race in self.checked_items:
+            self.checked_items.remove(race)
         else:
-            self.checked_items.append(checkbox)
+            self.checked_items.append(race)
         self.update_treeview()
 
-    # Function to update the treeview based on checked items
     def update_treeview(self):
         self.tree.delete(*self.tree.get_children())
         for item in self.checked_items:
@@ -232,7 +175,8 @@ class TreeviewApp(tk.Frame):
                     item['Distance'],
                     item['DistanceType'],
                     item['Handed']
-                ))
+                )
+            )
 
 
 # Run the app
